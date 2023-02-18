2a1530841702d6e90ee6a6f5318d228a2c18490d
user.email <rogerio.dosantos@ifpr.edu.br>
2023-02-18 11:58:42 -0300

    UpDate

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# escrevendo um título na página
+#Exemplo1 com emoji
+#st.title('Minha primeira aplicação :sunglasses:')
+#Exemplo2 com alerta canto superior direito
+st.title('Minha primeira aplicação :st.title:')
